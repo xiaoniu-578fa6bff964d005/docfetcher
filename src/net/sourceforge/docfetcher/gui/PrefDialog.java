fe3c35c763b9d2c5695b9e5052d8a2262f38be56
Tran Nam Quang <tran.nam.quang@web.de>
2011-10-29 19:41:12 +0200

    Preferences: Color chooser.

--- src/net/sourceforge/docfetcher/gui/PrefDialog.java
+++ src/net/sourceforge/docfetcher/gui/PrefDialog.java
@@ -30,9 +30,11 @@ import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.graphics.FontData;
+import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.ColorDialog;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
@@ -174,7 +176,7 @@ final class PrefDialog {
 	}
 	
 	@NotNull
-	public static StyledLabel createLabeledStyledLabel(	@NotNull Composite parent,
+	private static StyledLabel createLabeledStyledLabel(@NotNull Composite parent,
 														@NotNull String labelText) {
 		Label label = new Label(parent, SWT.NONE);
 		label.setText(labelText);
@@ -248,6 +250,7 @@ final class PrefDialog {
 	
 	private static final class ColorOption extends PrefOption {
 		@NotNull private SettingsConf.IntArray enumOption;
+		@NotNull private StyledText st;
 		@NotNull private Color color;
 		
 		public ColorOption(@NotNull String labelText, @NotNull SettingsConf.IntArray enumOption) {
@@ -259,26 +262,40 @@ final class PrefDialog {
 			label.setText(labelText);
 			label.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false));
 			
-			StyledText st = new StyledText(parent, SWT.SINGLE | SWT.BORDER);
-			st.setEditable(false);
+			int style = SWT.SINGLE | SWT.BORDER | SWT.READ_ONLY;
+			st = new StyledText(parent, style);
 			st.setCaret(null);
 			st.setCursor(st.getDisplay().getSystemCursor(SWT.CURSOR_HAND));
 			
 			GridData stGridData = new GridData(SWT.LEFT, SWT.CENTER, true, false);
 			stGridData.widthHint = 50;
 			st.setLayoutData(stGridData);
+			setColor(enumOption.get());
 			
-			int[] rgb = enumOption.get();
-			color = new Color(st.getDisplay(), rgb[0], rgb[1], rgb[2]);
-			st.setBackground(color);
-			st.setSelectionBackground(color);
-			st.setSelectionForeground(st.getForeground());
+			st.addMouseListener(new MouseAdapter() {
+				public void mouseDown(MouseEvent e) {
+					ColorDialog dialog = new ColorDialog(st.getShell());
+					dialog.setRGB(color.getRGB());
+					RGB rgb = dialog.open();
+					if (rgb == null)
+						return;
+					Color oldColor = color;
+					setColor(rgb.red, rgb.green, rgb.blue);
+					oldColor.dispose();
+				}
+			});
 			
 			st.addDisposeListener(new DisposeListener() {
 				public void widgetDisposed(DisposeEvent e) {
 					color.dispose();
 				}
 			});
+		}
+		private void setColor(@NotNull int... rgb) {
+			color = new Color(st.getDisplay(), rgb[0], rgb[1], rgb[2]);
+			st.setBackground(color);
+			st.setSelectionBackground(color);
+			st.setSelectionForeground(st.getForeground());
 		}
 	}
 
