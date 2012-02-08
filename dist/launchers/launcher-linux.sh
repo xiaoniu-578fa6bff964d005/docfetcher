32b20ec6b3b7ff8bf05c8c4744ec0adfa011ca96
Tran Nam Quang <tran.nam.quang@web.de>
2012-02-08 21:10:39 +0100

    Enabled assertions on the Windows launchers and raised the thread stack size on all launchers to 2 MB.

--- dist/launchers/launcher-linux.sh
+++ dist/launchers/launcher-linux.sh
@@ -10,4 +10,4 @@ do
    CLASSPATH=${CLASSPATH}:${FILE}
 done
 
-java -enableassertions -Xmx256m -cp ".:${CLASSPATH}" -Djava.library.path="lib" ${main_class} "$@"
+java -enableassertions -Xmx256m -Xss2m -cp ".:${CLASSPATH}" -Djava.library.path="lib" ${main_class} "$@"
