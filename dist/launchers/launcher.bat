7aab9432534c5958047a40e49ce879e2c5cde4ff
Nam-Quang Tran <tran.nam.quang@web.de>
2015-09-16 15:42:23 +0200

    Added comments to the DocFetcher.bat launcher.

--- dist/launchers/launcher.bat
+++ dist/launchers/launcher.bat
@@ -1,3 +1,15 @@
+rem  Note: This file is an alternative launcher for DocFetcher. How to use it:
+rem  1) Modify this file as needed.
+rem  2) Move this file one level up into the DocFetcher folder.
+rem  3) Double-click on this file to launch DocFetcher.
+rem  
+rem  Common modifications:
+rem  1) Replace the "java" keyword in the last line with the full path to the
+rem  Java executable.
+rem  2) Give DocFetcher more memory with the setting -Xmx..m in the last line.
+rem  For example, with -Xmx512m, DocFetcher will use up to 512 MB of memory.
+rem  Using more memory than about 1 GB requires a 64-bit Java runtime.
+
 @echo off
 
 cd %~dp0
