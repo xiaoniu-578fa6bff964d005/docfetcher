96ded737e2be9645a6c368e210dac8fb8eee3f77
Nam-Quang Tran <tran.nam.quang@web.de>
2015-10-31 16:24:54 +0100

    Fixed build tests and suppressed a logging message.

--- src/net/sourceforge/docfetcher/model/parse/MSOffice2007Parser.java
+++ src/net/sourceforge/docfetcher/model/parse/MSOffice2007Parser.java
@@ -12,28 +12,22 @@
 package net.sourceforge.docfetcher.model.parse;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collection;
 
 import net.sourceforge.docfetcher.enums.Msg;
 import net.sourceforge.docfetcher.enums.ProgramConf;
-import net.sourceforge.docfetcher.util.Util;
 import net.sourceforge.docfetcher.util.annotations.NotNull;
 
 import org.apache.poi.POITextExtractor;
 import org.apache.poi.extractor.ExtractorFactory;
-import org.apache.poi.openxml4j.exceptions.InvalidOperationException;
 import org.apache.poi.openxml4j.exceptions.OpenXML4JRuntimeException;
 import org.apache.poi.openxml4j.opc.OPCPackage;
 import org.apache.poi.openxml4j.opc.PackageAccess;
 import org.apache.poi.openxml4j.opc.PackageProperties;
 import org.apache.poi.xssf.extractor.XSSFExcelExtractor;
 
-import com.google.common.base.Strings;
-import com.google.common.base.Throwables;
 import com.google.common.io.Closeables;
-import com.google.common.io.Files;
 
 /**
  * @author Tran Nam Quang
@@ -72,37 +66,9 @@ abstract class MSOffice2007Parser extends FileParser {
 	@Override
 	protected ParseResult parse(File file, ParseContext context)
 			throws ParseException {
-		try {
-			return doParse(file, PackageAccess.READ);
-		}
-		catch (Exception e) {
-			Throwable root = Throwables.getRootCause(e);
-			if (root instanceof InvalidOperationException &&
-					Strings.nullToEmpty(root.getMessage()).contains("open in read only mode")) {
-				File tempFile = null;
-				try {
-					tempFile = Util.createTempFile(file.getName(), "." + Util.getExtension(file));
-					Files.copy(file, tempFile);
-					return doParse(tempFile, PackageAccess.READ_WRITE);
-				}
-				catch (IOException e1) {
-					throw new ParseException(e1);
-				}
-				finally {
-					if (tempFile != null) {
-						tempFile.delete();
-					}
-				}
-			} else {
-				throw new ParseException(e);
-			}
-		}
-	}
-	
-	private static ParseResult doParse(File file, PackageAccess access) throws ParseException {
 		OPCPackage pkg = null;
 		try {
-			pkg = OPCPackage.open(file.getPath(), access);
+			pkg = OPCPackage.open(file.getPath(), PackageAccess.READ);
 			String contents = extractText(pkg);
 			
 			// Open properties
@@ -197,39 +163,9 @@ abstract class MSOffice2007Parser extends FileParser {
 	@Override
 	protected final String renderText(File file, String filename)
 			throws ParseException {
-		try {
-			return doRenderText(file, PackageAccess.READ);
-		}
-		catch (Exception e) {
-			Throwable root = Throwables.getRootCause(e);
-			if (root instanceof InvalidOperationException &&
-					Strings.nullToEmpty(root.getMessage()).contains("open in read only mode")) {
-				File tempFile = null;
-				try {
-					tempFile = Util.createTempFile(file.getName(), "." + Util.getExtension(file));
-					Files.copy(file, tempFile);
-					return doRenderText(tempFile, PackageAccess.READ_WRITE);
-				}
-				catch (IOException e1) {
-					throw new ParseException(e1);
-				}
-				finally {
-					if (tempFile != null) {
-						tempFile.delete();
-					}
-				}
-			} else {
-				throw new ParseException(e);
-			}
-		}
-	}
-	
-	@NotNull
-	private static String doRenderText(File file, PackageAccess access)
-			throws ParseException {
 		OPCPackage pkg = null;
 		try {
-			pkg = OPCPackage.open(file.getPath(), access);
+			pkg = OPCPackage.open(file.getPath(), PackageAccess.READ);
 			return extractText(pkg);
 		}
 		catch (Exception e) {
--- src/net/sourceforge/docfetcher/model/parse/MSOffice2007ParserTest.java
+++ src/net/sourceforge/docfetcher/model/parse/MSOffice2007ParserTest.java
@@ -10,36 +10,22 @@ import net.sourceforge.docfetcher.model.index.IndexingConfig;
 import net.sourceforge.docfetcher.model.index.IndexingReporter;
 
 import org.apache.poi.extractor.ExtractorFactory;
-import org.apache.poi.openxml4j.exceptions.InvalidOperationException;
 import org.apache.poi.openxml4j.opc.OPCPackage;
 import org.apache.poi.openxml4j.opc.PackageAccess;
 import org.junit.Test;
 
-import com.google.common.base.Throwables;
-
 public final class MSOffice2007ParserTest {
 	
 	@Test
-	public void testReadOnlyFails() throws Exception {
+	public void testReadOnlySucceeds() throws Exception {
 		/*
-		 * In POI 3.10 FINAL, text extraction will fail on this file when opened 
-		 * in read-only mode, but succeed in read-write mode. The MS Office 2007
-		 * parser contains a workaround for this, and the latter would become
-		 * obsolete if in a later POI version the file could be read in
-		 * read-only mode.
+		 * In POI 3.10 FINAL, text extraction failed on this file when opened in
+		 * read-only mode, but succeed in read-write mode. In POI 3.13, this bug
+		 * has apparently been fixed.
 		 */
 		File file = TestFiles.read_write_xls.get();
 		OPCPackage pkg = OPCPackage.open(file.getPath(), PackageAccess.READ);
-		try {
-			ExtractorFactory.createExtractor(pkg).getText();
-		}
-		catch (Exception e) {
-			Throwable root = Throwables.getRootCause(e);
-			String msg = ((InvalidOperationException) root).getMessage();
-			Assert.assertTrue(msg.contains("read only"));
-			return;
-		}
-		Assert.assertTrue(false);
+		ExtractorFactory.createExtractor(pkg).getText();
 	}
 	
 	@Test
