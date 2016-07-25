8544f33ff652decd8c10e5f822b01dbe4f7603b2
Nam-Quang Tran <tran.nam.quang@web.de>
2016-07-25 16:33:14 +0200

    With SWT 4.6 on OS X, the file icons on the result pane were too big.

--- src/net/sourceforge/docfetcher/util/gui/FileIconCache.java
+++ src/net/sourceforge/docfetcher/util/gui/FileIconCache.java
@@ -18,11 +18,14 @@ import net.sourceforge.docfetcher.util.Util;
 import net.sourceforge.docfetcher.util.annotations.NotNull;
 import net.sourceforge.docfetcher.util.annotations.Nullable;
 
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.DisposeEvent;
 import org.eclipse.swt.events.DisposeListener;
+import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.ImageData;
 import org.eclipse.swt.program.Program;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Widget;
 
 import com.google.common.collect.Maps;
@@ -35,6 +38,11 @@ import com.google.common.collect.Maps;
  * @author Tran Nam Quang
  */
 public final class FileIconCache {
+	
+	/**
+	 * Icons with width or height greater than this value will be scaled down.
+	 */
+	private static final int maxSize = 16;
 	
 	private final Map<String, Image> fileIconMap = Maps.newHashMap();
 	private final Widget disposeWidget;
@@ -77,9 +85,28 @@ public final class FileIconCache {
 		ImageData imgData = program.getImageData();
 		if (imgData == null)
 			return defaultIcon;
-		image = new Image(disposeWidget.getDisplay(), imgData);
+		
+		/* With SWT 4.6 on OS X, Program.findProgram returns huge icons, so
+		 * we'll have to scale them down. */
+		if (imgData.width > maxSize || imgData.height > maxSize) {
+			image = scaleTo(imgData, disposeWidget.getDisplay(), maxSize, maxSize);
+		} else {
+			image = new Image(disposeWidget.getDisplay(), imgData);
+		}
 		fileIconMap.put(extension, image);
 		return image;
+	}
+	
+	private static Image scaleTo(ImageData imgData, Display display, int width, int height) {
+		Image image1 = new Image(display, imgData);
+		Image image2 = new Image(display, width, height);
+		GC gc = new GC(image2);
+		gc.setAntialias(SWT.ON);
+		gc.setInterpolation(SWT.HIGH);
+		gc.drawImage(image1, 0, 0, imgData.width, imgData.height, 0, 0, width, height);
+		gc.dispose();
+		image1.dispose();
+		return image2;
 	}
 
 }
